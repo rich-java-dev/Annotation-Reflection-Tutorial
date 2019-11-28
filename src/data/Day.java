76468fca39dd6ff5e4e998bd11dbc936bb39c069
Richard White <RichW@edmundsassoc.com>
2019-11-27 21:09:01 -0500

    remote sql.Date import/squeeze code to under 200 lines

--- src/data/Day.java
+++ src/data/Day.java
@@ -1,11 +1,9 @@
 package data;
 
-import java.sql.Date;
-
 public class Day extends Data {
 
   public Day(int yyyy, int mm, int dd) {
-    this.val = Date.valueOf(yyyy + "-" + mm + "-" + dd);
+    this.val = mm + "/" + dd + "/" + yyyy;
   }
 
 }
